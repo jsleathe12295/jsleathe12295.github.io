8e2da47a0d29bb0168cde685439897836ef35324
jsleathe12295 <jonathan.leathe@gordon.edu>
2016-10-05 14:16:38 -0400

    fixing routes

--- app/js/website.js
+++ app/js/website.js
@@ -1,7 +1,22 @@
-angular.module('MyWebsite',[]).
-    config(function($routeProvider){
+
+
+angular.module('MyWebsite',['ngRoute'])
+.config(function($routeProvider) {
+        $routeProvider
+            // route for the home page/signin page
+            .when('/', {
+                template : require('./pages/signin.html'),
+                controller  : 'AuthController as AU',
+                css : 'styles.css'
+            })
+
+
+
+
+
+    config(function('$routeProvider'){
         $routeProvider.
-          when('/about',{template:'views/about.html'}).
+          when('/about',{templateUrl:'views/about.html'}).
           otherwise({redirectTo:'/home', template:'views/home.html'});
     });
 
