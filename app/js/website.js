60e22fc207590787f52058751294773927b40465
jsleathe12295 <jonathan.leathe@gordon.edu>
2016-10-04 23:04:08 -0400

    home page

--- app/js/website.js
+++ app/js/website.js
@@ -2,7 +2,7 @@ angular.module('MyWebsite',[]).
     config(function($routeProvider){
         $routeProvider.
           when('/about',{template:'views/about.html'}).
-          otherwise({redirectTo:'/home', template:'views/index.html'});
+          otherwise({redirectTo:'/home', template:'views/home.html'});
     });
 
 
