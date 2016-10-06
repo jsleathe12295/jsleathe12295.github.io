efcad309648a920e37186a18a9cd81840b274b9c
jsleathe12295 <jonathan.leathe@gordon.edu>
2016-10-05 20:45:53 -0400

    adding script for angular

--- app/app.js
+++ app/app.js
@@ -1,4 +1,10 @@
-angular.module('MyWebsite', ['ngRoute']);
+var MyWebsite = angular.module('MyWebsite', ['ngRoute'])
+.config(['routeProvider', function($routeProvider)]) {
+        $routeProvider
+            // route for the home page/signin page
+          routeProvider.when('/home', {templateUrl:'pages/home.html' });
+          routeProvider.when('/about', {templateUrl:'pages/about.html' });
+          routeProvider.otherwise({redirectTo: '/home'} });
 
-require('app/controllers');
-require('app/routes');
+
+            });
