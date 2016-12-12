8212c106479d67bb12f8ed349b7686d40292b6e6
jsleathe12295 <jonathan.leathe@gordon.edu>
2016-12-11 19:13:31 -0500

    fixing display

--- public/app.js
+++ public/app.js
@@ -1,24 +1,23 @@
 var MyWebsite = angular.module('MyWebsite', ['ngRoute']);
 MyWebsite.config(function($routeProvider, $locationProvider) {
-        $routeProvider
-            // route for the home page
-            .when('/', {
-    				templateUrl : 'pages/home.html',
-    				controller  : 'mainController'
-    			}),
+  $routeProvider
+  // route for the home page
+    .when('/', {
+			templateUrl : 'pages/home.html',
+			controller  : 'mainController'
+		})
 
-    			// route for the about page
-    			.when('/about', {
-    				templateUrl : 'pages/about.html',
-    				controller  : 'aboutController'
-            // css: 'style.css'
-    			})
+		// route for the about page
+		  .when('/about', {
+			templateUrl : 'pages/about.html',
+			controller  : 'aboutController'
+		})
 
-           $locationProvider.html5Mode(true);
+     $locationProvider.html5Mode(true);
     });
 
 
-    MyWebsite.controller('mainController', function($scope) {
+  MyWebsite.controller('mainController', function($scope) {
 		// create a message to display in our view
 		$scope.message = 'Under Maintainence';
 	});
